4a4a3d2d2dc5dd001680fd54d017c67cf48aba61
francooo32 <63883342+francooo32@users.noreply.github.com>
2023-02-25 16:07:36 -0300

    Added attachments structure and file size limit

--- controller/mailcontroller.js
+++ controller/mailcontroller.js
@@ -5,11 +5,13 @@ exports.sendMail=(req, res)=>{
     let userMail = req.body.email;
     let userMsj = req.body.msj;
     let fileBase64 = req.body.base64
-
-    if(fileBase64 != null)
-        const attachments = fileBase64.map((base64)=>{
+    let attachments = "";
+    if(fileBase64 != null){
+        attachments = fileBase64.map((base64)=>{
             return { path: base64 };
         });
+    }else{
+        attachments = [{ path: base64 }];
     }
 
     let transporter = nodeMailer.createTransport({
